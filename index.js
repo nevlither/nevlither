caec16a398f1a9ea241fc7ec4ec4e56eabf2b6b6
nevlither <122714025+nevlither@users.noreply.github.com>
2026-03-28 21:01:23 -0500

    boolean

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ document.getElementById("pone").textContent = "More test wow";
 
 // comment
 
-let x = 100;
+//let x = 100;
 
-console.log(x);
-console.log(`One has ${x} stuff`);
-console.log(`This has ${x}`)
+//console.log(x);
+//console.log(`One has ${x} stuff`);
+//console.log(`This has ${x}`)
+
+let online = true;
+let go = false;
+console.log(typeof online);
+console.log(`Go: ${go}})
